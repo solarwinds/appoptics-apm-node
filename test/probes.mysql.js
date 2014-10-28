8ce3ac7972c724bf1919cb4eee491beb67304387
Stephen Belanger <sbelanger@appneta.com>
2014-10-27 17:30:03 -0700

    [mysql] Make compatible with v0.9.x of mysql module

--- test/probes.mysql.js
+++ test/probes.mysql.js
@@ -1,12 +1,16 @@
+// require('longjohn')
+
 var debug = require('debug')('probes-mysql')
 var helper = require('./helper')
 var should = require('should')
-var tv = require('..')
+var semver = require('semver')
+var tv = global.tv = require('..')
 var addon = tv.addon
 
 var request = require('request')
 var http = require('http')
 
+var pkg = require('mysql/package.json')
 var mysql = require('mysql')
 
 describe('probes.mysql', function () {
@@ -42,33 +46,65 @@ describe('probes.mysql', function () {
     }
   }
 
-  beforeEach(function (done) {
-    db = ctx.mysql = mysql.createConnection({
-      host: 'localhost',
-      database: 'test',
-      user: 'root'
+  if (semver.satisfies(pkg.version, '>= 2.0.0')) {
+    beforeEach(function (done) {
+      db = ctx.mysql = mysql.createConnection({
+        host: 'localhost',
+        database: 'test',
+        user: 'root'
+      })
+
+      // Set pool and pool cluster
+      var poolConfig = {
+        connectionLimit: 10,
+        host: 'localhost',
+        database: 'test',
+        user: 'root'
+      }
+
+      pool = db.pool = mysql.createPool(poolConfig)
+      cluster = db.cluster = mysql.createPoolCluster()
+      cluster.add(poolConfig)
+
+      // Connect
+      db.connect(done)
     })
+    afterEach(function (done) {
+      db.end(done)
+    })
+  } else if (semver.satisfies(pkg.version, '>= 0.9.2')) {
+    beforeEach(function () {
+      db = ctx.mysql = mysql.createClient({
+        host: 'localhost',
+        database: 'test',
+        user: 'root'
+      })
+    })
+  } else {
+    beforeEach(function (done) {
+      db = ctx.mysql = new mysql.Client({
+        host: 'localhost',
+        database: 'test',
+        user: 'root'
+      })
+      db.connect(done)
+    })
+  }
 
-    // Set pool and pool cluster
-    var poolConfig = {
-      connectionLimit: 10,
-      host: 'localhost',
-      database: 'test',
-      user: 'root'
-    }
+  it('should trace a basic query', test_basic)
+  it('should trace a query with a value list', test_values)
+  it('should trace a streaming query', test_stream)
 
-    pool = db.pool = mysql.createPool(poolConfig)
-    cluster = db.cluster = mysql.createPoolCluster()
-    cluster.add(poolConfig)
+  if (semver.satisfies(pkg.version, '>= 2.0.0')) {
+    it('should trace a pooled query', test_pool)
+    it('should trace a cluster pooled query', test_clustered_pool)
+  } else {
+    it.skip('should trace a pooled query', test_pool)
+    it.skip('should trace a cluster pooled query', test_clustered_pool)
+  }
+  it('should sanitize a query', test_sanitize)
 
-    // Connect
-    db.connect(done)
-  })
-  afterEach(function (done) {
-    db.end(done)
-  })
-
-  it('should trace a basic query', function (done) {
+  function test_basic (done) {
     helper.httpTest(emitter, helper.run(ctx, 'mysql/basic'), [
       function (msg) {
         checks.entry(msg)
@@ -78,9 +114,14 @@ describe('probes.mysql', function () {
         checks.exit(msg)
       }
     ], done)
-  })
+  }
 
-  it('should trace a query with a value list', function (done) {
+  function test_values (done) {
+    var last = tv.Layer.last
+    if (last) {
+      console.log('last is', last)
+    }
+
     helper.httpTest(emitter, helper.run(ctx, 'mysql/values'), [
       function (msg) {
         checks.entry(msg)
@@ -91,9 +132,9 @@ describe('probes.mysql', function () {
         checks.exit(msg)
       }
     ], done)
-  })
+  }
 
-  it('should trace a streaming query', function (done) {
+  function test_stream (done) {
     helper.httpTest(emitter, helper.run(ctx, 'mysql/stream'), [
       function (msg) {
         checks.entry(msg)
@@ -103,9 +144,9 @@ describe('probes.mysql', function () {
         checks.exit(msg)
       }
     ], done)
-  })
+  }
 
-  it('should trace a pooled query', function (done) {
+  function test_pool (done) {
     helper.httpTest(emitter, helper.run(ctx, 'mysql/pool'), [
       function (msg) {
         checks.entry(msg)
@@ -115,9 +156,9 @@ describe('probes.mysql', function () {
         checks.exit(msg)
       }
     ], done)
-  })
+  }
 
-  it('should trace a cluster pooled query', function (done) {
+  function test_clustered_pool (done) {
     helper.httpTest(emitter, helper.run(ctx, 'mysql/pool-cluster'), [
       function (msg) {
         checks.entry(msg)
@@ -127,9 +168,9 @@ describe('probes.mysql', function () {
         checks.exit(msg)
       }
     ], done)
-  })
+  }
 
-  it('should sanitize a query', function (done) {
+  function test_sanitize (done) {
     helper.httpTest(emitter, helper.run(ctx, 'mysql/sanitize'), [
       function (msg) {
         checks.entry(msg)
@@ -139,6 +180,6 @@ describe('probes.mysql', function () {
         checks.exit(msg)
       }
     ], done)
-  })
+  }
 
 })
