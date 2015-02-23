d8364152642005c48e6981851ff8840a95022738
Stephen Belanger <sbelanger@appneta.com>
2015-02-23 15:52:08 -0800

    [oracle] Add tests for pooling and isAutoCommit inclusion. Also, fixed an issue with things getting patched multiple times

--- test/probes/oracledb.test.js
+++ test/probes/oracledb.test.js
@@ -59,20 +59,100 @@ describe('probes.oracledb', function () {
 
   if (oracledb && host && database && config.user && config.password) {
     it('should trace execute calls', test_basic)
+    it('should trace execute calls in pool', test_pool)
+    it('should include correct isAutoCommit value', test_commit)
   } else {
     it.skip('should trace execute calls', test_basic)
+    it.skip('should trace execute calls in pool', test_pool)
+    it.skip('should include correct isAutoCommit value', test_commit)
   }
 
   function test_basic (done) {
     helper.httpTest(emitter, function (done) {
-      oracledb.getConnection(config, function (err, connection) {
+      function query (err, connection) {
         if (err) return done(err)
         connection.execute('SELECT 1 FROM DUAL', done)
+      }
+
+      oracledb.getConnection(config, query)
+    }, [
+      function (msg) {
+        checks['oracle-entry'](msg)
+        msg.should.have.property('Query', 'SELECT 1 FROM DUAL')
+      },
+      function (msg) {
+        checks['oracle-exit'](msg)
+      }
+    ], done)
+  }
+
+  function test_pool (done) {
+    helper.httpTest(emitter, function (done) {
+      function query (err, connection) {
+        if (err) return done(err)
+        connection.execute('SELECT 1 FROM DUAL', done)
+      }
+
+      oracledb.createPool(config, function (err, pool) {
+        if (err) return done(err)
+        pool.getConnection(query)
       })
     }, [
       function (msg) {
         checks['oracle-entry'](msg)
         msg.should.have.property('Query', 'SELECT 1 FROM DUAL')
+      },
+      function (msg) {
+        checks['oracle-exit'](msg)
+      }
+    ], done)
+  }
+
+  function test_commit (done) {
+    helper.httpTest(emitter, function (done) {
+      oracledb.isAutoCommit = false
+      oracledb.getConnection(config, function (err, connection) {
+        function query (isAutoCommit, done) {
+          var options = {
+            isAutoCommit: isAutoCommit
+          }
+
+          return function (err) {
+            if (err) return done(err)
+            connection.execute('SELECT 1 FROM DUAL', [], options, done)
+          }
+        }
+
+        var fn = query(
+          undefined,
+          query(
+            true,
+            query(
+              false,
+              done
+            )
+          )
+        )
+        fn()
+      })
+    }, [
+      function (msg) {
+        checks['oracle-entry'](msg)
+        msg.should.have.property('isAutoCommit', false)
+      },
+      function (msg) {
+        checks['oracle-exit'](msg)
+      },
+      function (msg) {
+        checks['oracle-entry'](msg)
+        msg.should.have.property('isAutoCommit', true)
+      },
+      function (msg) {
+        checks['oracle-exit'](msg)
+      },
+      function (msg) {
+        checks['oracle-entry'](msg)
+        msg.should.have.property('isAutoCommit', false)
       },
       function (msg) {
         checks['oracle-exit'](msg)
