7b4d3f03c1f523ac544df785b3762eb9111bd0c2
Stephen Belanger <cyruzdraxs@gmail.com>
2015-07-06 12:27:34 -0700

    bluebird: Fix domain bind issue

---
+++ test/probes/bluebird.test.js
@@ -0,0 +1,37 @@
+var helper = require('../helper')
+var tv = helper.tv
+
+var Promise = require('bluebird')
+var domain = require('domain')
+
+function delay (n) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, n)
+  })
+}
+
+describe('probes/bluebird', function () {
+  it('should support promises', function (done) {
+    tv.requestStore.run(function () {
+      tv.requestStore.set('foo', 'bar')
+      delay(100).then(function () {
+        tv.requestStore.get('foo').should.equal('bar')
+        done()
+      }, done)
+    })
+  })
+
+  it('should support promises in domains', function (done) {
+    var d = domain.create()
+    d.on('error', done)
+    d.run(function () {
+      tv.requestStore.run(function () {
+        tv.requestStore.set('foo', 'bar')
+        delay(100).then(function () {
+          tv.requestStore.get('foo').should.equal('bar')
+          done()
+        }, done)
+      })
+    })
+  })
+})
