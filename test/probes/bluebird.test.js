fbb1f2ccb9837bae81cf2b5d06fdc171dcac6854
Stephen Belanger <cyruzdraxs@gmail.com>
2015-07-06 15:41:11 -0700

    bluebird: Fake layer presence

--- test/probes/bluebird.test.js
+++ test/probes/bluebird.test.js
@@ -13,6 +13,9 @@ function delay (n) {
 describe('probes/bluebird', function () {
   it('should support promises', function (done) {
     tv.requestStore.run(function () {
+      // Hack to look like there's a previous layer
+      tv.requestStore.set('lastLayer', true)
+
       tv.requestStore.set('foo', 'bar')
       delay(100).then(function () {
         tv.requestStore.get('foo').should.equal('bar')
@@ -26,6 +29,9 @@ describe('probes/bluebird', function () {
     d.on('error', done)
     d.run(function () {
       tv.requestStore.run(function () {
+        // Hack to look like there's a previous layer
+        tv.requestStore.set('lastLayer', true)
+
         tv.requestStore.set('foo', 'bar')
         delay(100).then(function () {
           tv.requestStore.get('foo').should.equal('bar')
