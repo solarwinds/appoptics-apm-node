fe1cf69b12f5c7772d17cea44b16681e961180b0
Stephen Belanger <sbelanger@appneta.com>
2016-02-11 16:42:58 -0800

    layer, event, error: Fix internal event chaining
    This fixes makes internal events chain properly, makes sub layers
    link correctly around internal events and expands the test suite
    around the behaviour of how internal events work and the custom
    instrumentation interfaces to create them manually.

--- test/layer.test.js
+++ test/layer.test.js
@@ -3,6 +3,7 @@ var should = require('should')
 var tv = require('..')
 var addon = tv.addon
 var Layer = tv.Layer
+var Event = tv.Event
 
 describe('layer', function () {
   var emitter
@@ -298,17 +299,229 @@ describe('layer', function () {
     }
 
     var checks = [
+      function () {},
       function (msg) {
+        msg.should.not.have.property('Layer')
         msg.should.have.property('Label', 'info')
-
         Object.keys(data).forEach(function (key) {
           msg.should.have.property(key, data[key])
         })
-      }
+      },
+      function () {}
     ]
 
     helper.doChecks(emitter, checks, done)
 
-    layer.info(data)
+    layer.run(function () {
+      layer.info(data)
+    })
   })
+
+  it('should not send info events when not in a layer', function () {
+    var layer = new Layer('test', null, {})
+    var data = { Foo: 'bar' }
+
+    var send = Event.prototype.send
+    Event.prototype.send = function () {
+      Event.prototype.send = send
+      throw new Error('should not send when not in a layer')
+    }
+
+    layer.info(data)
+    Event.prototype.send = send
+  })
+
+  it('should allow sending the same info data multiple times', function (done) {
+    var layer = new Layer('test', null, {})
+    var e = layer.events.entry
+    var data = {
+      Foo: 'bar'
+    }
+
+    function check (msg) {
+      msg.should.not.have.property('Layer')
+      msg.should.have.property('Label', 'info')
+      Object.keys(data).forEach(function (key) {
+        msg.should.have.property(key, data[key])
+      })
+    }
+
+    helper.doChecks(emitter, [
+      function () {},
+      check,
+      check,
+      function () {}
+    ], done)
+
+    layer.run(function () {
+      layer.info(data)
+      layer.info(data)
+    })
+  })
+
+  it('should fail silently when sending non-object-literal info', function () {
+    var layer = new Layer('test', null, {})
+    layer._internal = function () {
+      throw new Error('should not have triggered an _internal call')
+    }
+    layer.info(undefined)
+    layer.info(new Date)
+    layer.info(/foo/)
+    layer.info('wat')
+    layer.info(null)
+    layer.info([])
+    layer.info(1)
+  })
+
+  it('should chain internal event edges', function (done) {
+    var layer = new Layer('test', null, {})
+
+    var n = 10 + Math.floor(Math.random() * 10)
+    var msgs = []
+
+    function push (msg) {
+      msgs.push(msg)
+    }
+
+    var checks = [ push, push ]
+    for (var i = 0; i < n; i++) {
+      checks.push(push)
+    }
+
+    helper.doChecks(emitter, checks, function (err) {
+      if (err) return done(err)
+      var prev = msgs.shift()
+      msgs.forEach(function (event) {
+        linksTo(event, prev)
+        prev = event
+      })
+      done()
+    })
+
+    function sendAThing (i) {
+      if (Math.random() > 0.5) {
+        layer.error(new Error('error ' + i))
+      } else {
+        layer.info({ index: i })
+      }
+    }
+
+    layer.run(function () {
+      for (var i = 0; i < n; i++) {
+        sendAThing(i)
+      }
+    })
+  })
+
+  it('should chain internal events around sync sub layer', function (done) {
+    var layer = new Layer('outer', null, {})
+
+    var before = { state: 'before' }
+    var after = { state: 'after' }
+
+    var track = edgeTracker()
+
+    var checks = [
+      checkEntry('outer', track),
+        checkInfo(before, track),
+        checkEntry('inner', track),
+        checkExit('inner', track),
+        checkInfo(after, track),
+      checkExit('outer', track)
+    ]
+
+    helper.doChecks(emitter, checks, done)
+
+    layer.run(function () {
+      layer.info(before)
+      layer.descend('inner').run(function () {
+        // Do nothing
+      })
+      layer.info(after)
+    })
+  })
+
+  it('should chain internal events around async sub layer', function (done) {
+    var layer = new Layer('outer', null, {})
+
+    var before = { state: 'before' }
+    var after = { state: 'after' }
+
+    var trackOuter = edgeTracker()
+    var trackInner = edgeTracker()
+
+    var checks = [
+      checkEntry('outer', trackOuter),
+        checkInfo(before, trackOuter),
+
+        // Async call
+        checkEntry('inner', trackInner),
+        checkInfo(before, trackInner),
+
+        checkInfo(after, trackOuter),
+      checkExit('outer', trackOuter),
+
+        // Next tick
+        checkInfo(after, trackInner),
+        checkExit('inner', trackInner)
+    ]
+
+    helper.doChecks(emitter, checks, done)
+
+    layer.run(function () {
+      layer.info(before)
+      var sub = layer.descend('inner')
+      sub.run(function (wrap) {
+        var cb = wrap(function () {})
+        setImmediate(function () {
+          tv.reportInfo(after)
+          cb()
+        })
+        tv.reportInfo(before)
+      })
+      layer.info(after)
+    })
+  })
+
 })
+
+function linksTo (a, b) {
+  a.Edge.should.eql(b['X-Trace'].substr(42))
+}
+
+function edgeTracker () {
+  var last = null
+  return function (msg) {
+    if (last) linksTo(msg, last)
+    last = msg
+  }
+}
+
+function checkEntry (name, fn) {
+  return function (msg) {
+    msg.should.have.property('X-Trace')
+    msg.should.have.property('Label', 'entry')
+    msg.should.have.property('Layer', name)
+    if (fn) fn(msg)
+  }
+}
+
+function checkExit (name, fn) {
+  return function (msg) {
+    msg.should.have.property('X-Trace')
+    msg.should.have.property('Label', 'exit')
+    msg.should.have.property('Layer', name)
+    if (fn) fn(msg)
+  }
+}
+
+function checkInfo (data, fn) {
+  return function (msg) {
+    msg.should.not.have.property('Layer')
+    msg.should.have.property('Label', 'info')
+    Object.keys(data).forEach(function (key) {
+      msg.should.have.property(key, data[key])
+    })
+    if (fn) fn(msg)
+  }
+}
