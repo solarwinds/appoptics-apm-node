a3b941550786dc9a242acaafcdf503eb44747bc5
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2017-09-23 08:23:55 -0700

    Add repository env var. Add AO prefix to env vars.

--- install-appoptics-bindings.js
+++ install-appoptics-bindings.js
@@ -1,17 +1,32 @@
 'use strict'
 
+//
+// this file exists so that authorization can be add to the package specifier
+// to enable access to private repositories or alternate branches.
+//
+//
+//
+
 var spawn = require('child_process').spawn
 
-var suffix = 'librato/node-appoptics-bindings.git#single-step-install'
+var suffix = 'node-appoptics-bindings'
 var prefix = ''
 var env = process.env
 
-if (env.TEST_GITAUTH) {
-    prefix = 'https://' + env.TEST_GITAUTH + ':x-oauth-basic@github.com/'
-} else if (env.TEST_GITUSER && env.TEST_GITPASS) {
-    prefix = 'https://' + env.TEST_GITUSER + ':' + env.TEST_GITPASS + '@github.com/'
+if (env.AO_TEST_PACKAGE) {
+  // use the specified package
+  suffix = env.AO_TEST_PACKAGE
 }
 
-var status = spawn('npm', ['install', prefix + suffix], {stdio: 'inherit'})
+if (env.AO_TEST_GITAUTH) {
+  // an authentication token is specified
+  prefix = 'https://' + env.AO_TEST_GITAUTH + ':x-oauth-basic@github.com/'
+
+} else if (env.AO_TEST_GITUSER && env.AO_TEST_GITPASS) {
+  // a user and password are specified
+  prefix = 'https://' + env.AO_TEST_GITUSER + ':' + env.AO_TEST_GITPASS + '@github.com/'
+}
+
+var status = spawn('npm', ['install', prefix + suffix], { stdio: 'inherit' })
 
 process.exit(status)
