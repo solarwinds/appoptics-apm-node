26bed77f9b4147544d5935735324a4d76beddcb0
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-05-03 06:37:07 -0700

    Update for node version > 4 only.

--- test/versions.js
+++ test/versions.js
@@ -3,56 +3,44 @@
 const semver = require('semver')
 const modules = module.exports = []
 
-test('amqp', '>= 0.1.8')
-test('amqplib', '>= 0.2.0')
+//
+// using a minimum version can avoid testing
+// versions known to fail, speeding the test.
+//
+test('amqp', '>= 0.2.0')
+test('amqplib', '>= 0.2.0 < 0.5.0 || > 0.5.0')
 
-test('bcrypt',              '>= 0.7.4')
+test('bcrypt', '>= 0.8.5')
 test('bluebird')
 
-test('cassandra-driver',    '>= 0.2.0')
+test('cassandra-driver', '>= 3.3.0')
 test('co-render')
-test('director',            '>= 1.1.10')
-test('express',             '>= 3.0.0')
+test('director', '>= 1.1.10')
+test('express', '>= 3.0.0')
 
-test('generic-pool',        '>= 1.0.3')
+test('generic-pool', '>= 2.4.0')
 
-// Exclude 8.3.0 and 9.0.0 due to missing dependency bugs
-test('hapi',                [
-  '>= 6 < 8.3 || >= 8.3.1 < 9 || >= 9.0.1',
-  version('>= 4.0.0') ? '>= 9.0.1' : '>= 9.0.1 < 11'
-])
+test('hapi', '>= 9.0.1')
 test('koa-resource-router')
-test('koa-route',           '>= 1.0.1')
-test('koa-router',          '>= 1.6.0')
+test('koa-route', '>= 1.0.1')
+test('koa-router', '>= 1.6.0')
 test('koa')
-test('levelup',             '>= 0.17.0')
-test('memcached', version('>= 4.0.0') ? [
-                            '>= 0.1.1 < 1.0.0 || >= 2.2.0'
-] : version('>= 0.12.0') ? [
-                            '>= 0.1.1 < 1.0.0 || >= 2.1.0'
-] : [
-                            '>= 0.1.1'
-])
+test('levelup', '>= 0.17.0')
+test('memcached', '>= 2.2.0')
 
 // MongoDB 2.x support is handle via mongodb-core instrumentation
 test('mongodb-core', '>= 1.1.0')
 
-test('mongoose',     '>= 2.2.1 < 4.2 || >= 4.2.2')
+test('mongoose', '>= 2.2.1 < 4.2 || >= 4.2.2')
 
-test('mysql',               '>= 2.0.0')
-test('oracledb')
+test('mysql', '>= 2.0.0')
+test('oracledb', '>=2.0.0')
 
-// Exclude versions older than 2.8.4 on newer node versions,
-// as the native module in older versions is not compatible.
-test('pg', version('>= 0.12.0') ? [
-                            '>= 2.8.4'
-] : [
-                            '>= 0.13.3'
-])
+test('pg', '>= 2.8.4')
 test('raw-body')
-test('redis',               '>= 0.8.0')
-test('restify',             '>= 2.0.0 < 2.0.2 || >= 2.0.3')
-test('tedious',             '>= 0.1.5')
+test('redis', '>= 0.8.0')
+test('restify', '>= 2.0.0 < 2.0.2 || >= 2.0.3')
+test('tedious', '>= 0.1.5')
 
 
 //
