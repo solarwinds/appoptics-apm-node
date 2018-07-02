8abe15e46bc12420debff5f42791d8fedae6254f
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-07-02 08:45:54 -0700

    Move failure-prone deps installs to deps.sh. Remove postinstall of bindings.

--- deps.sh
+++ deps.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+# don't even try install these in production
+[ $NODE_ENV = "production" ] && exit 0
+
+# they're all optional, so ignore failures
 {
-  npm install oracledb@0.3.1
+    npm install oracledb@0.3.1
 } || {
-  exit 0
+    echo "unable to install optional oracledb package"
+}
+
+{
+    npm install pg@4.5.7
+} || {
+    echo "unable to install optional pg package"
+}
+
+{
+    npm install pg-native@1.10.0
+} || {
+    echo "unable to install optional pg-native package"
 }
