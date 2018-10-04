bb60eac9685dc253e5d49e5127353b6dd121e5c6
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-10-04 06:45:49 -0700

    Implement new version syntax for hapi.

--- test/versions.js
+++ test/versions.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const semver = require('semver')
-const modules = module.exports = []
+const packages = module.exports = []
 
 //
 // using a minimum version can avoid testing versions
@@ -20,7 +20,20 @@ test('express', '>= 3.0.0')
 
 test('generic-pool', '>= 2.4.0')
 
-test('hapi', '>= 9.0.1')
+//test('hapi', '>= 9.0.1')
+//test2('hapi', '>= 9.0.1')
+test2('hapi', {
+  ranges: [
+    {
+      range: '>= 9.0.1 < 17.0.0',
+      dependencies: ['vision@4'],
+    }, {
+      range: '>= 17.0.0',
+      dependencies: ['vision@5'],
+    }
+  ]
+})
+
 test('koa-resource-router')
 test('koa-route', '>= 1.0.1')
 test('koa-router', '>= 1.6.0')
@@ -48,7 +61,8 @@ test('tedious', '>= 0.1.5')
 //
 
 function test (name, range, task) {
-  modules.push({
+  packages.push({
+    version: 1,
     name: name,
     task: task || './node_modules/gulp/bin/gulp.js test:probe:' + name,
     range: range || '*',
@@ -56,6 +70,35 @@ function test (name, range, task) {
   })
 }
 
-function version (range) {
-  return semver.satisfies(process.versions.node, range)
+function test2 (name, options = {}) {
+  const task = options.task || './node_modules/gulp/bin/gulp.js test:probe:' + name
+  const timeout = options.timeout || 1000 * 60
+
+  let ranges
+  if (typeof options === 'string') {
+    ranges = [{range: options}]
+  } else if (!options.ranges) {
+    ranges = [{range: '*'}]
+  } else if (typeof options.ranges === 'string') {
+    ranges = [{range: options.ranges}]
+  } else if (Array.isArray(options.ranges)) {
+    ranges = options.ranges
+  } else {
+    throw new Error(`Unexpected range ${options.range} for package ${name} in versions file ${__filename}`)
+  }
+
+  // consider checking whether ranges overlap or are out of order.
+  /*
+  for (let i = 0; i < ranges.length - 1; i++) {
+
+  }
+  // */
+
+  packages.push({
+    version: 2,
+    name,
+    task,
+    ranges,
+    timeout,
+  })
 }
