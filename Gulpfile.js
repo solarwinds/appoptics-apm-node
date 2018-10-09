50ef93d07ffb557411c75481a871595ad6d7c786
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-10-09 16:20:50 -0700

    Gulp back to v3.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,9 +4,19 @@ const fs = require('fs')
 const path = require('path')
 const gulp = require('gulp')
 const mocha = require('gulp-mocha')
-//const istanbul = require('gulp-istanbul')
+const istanbul = require('gulp-istanbul')
 const spawn = require('child_process').spawn
+const mkdirp = require('mkdirp')
 
+// Ensure existence of dist and probe directories
+/*
+gulp.task('dist', function (cb) {
+  mkdirp('dist', cb)
+})
+gulp.task('dist/probes', ['dist'], function (cb) {
+  mkdirp('dist/probes', cb)
+})
+// */
 
 // Describe basic tasks and their associated files
 const tasks = {
@@ -25,15 +35,8 @@ const tasks = {
   }
 }
 
-// group the tasks.
-const taskGroups = {}
-Object.keys(tasks).forEach(k => {
-  taskGroups[k] = []
-})
-
 // Make individual unit test tasks
 const unitTests = fs.readdirSync('test/')
-
 unitTests.forEach(function (file) {
   if (!/.+\.test\.js$/.test(file)) return
 
@@ -44,7 +47,6 @@ unitTests.forEach(function (file) {
     test: 'test/' + file,
     bench: 'test/' + name + '.bench.js'
   }
-  taskGroups.unit.push('unit:' + name)
 })
 
 // some probes run a different named test. this is important
@@ -59,10 +61,8 @@ const nameMap = {
 
 // Describe probe tasks automatically
 const probes = fs.readdirSync('lib/probes')
-
 probes.forEach(function (probe) {
   let name = probe.replace(/\.js$/, '')
-
   if (name in nameMap) {
     name = nameMap[name]
   }
@@ -73,14 +73,12 @@ probes.forEach(function (probe) {
   const test = 'test/probes/' + name + '.test.js'
   if (fs.existsSync(test)) {
     task.test = test
-    taskGroups.probes.push('probe:' + name)
   }
 
   const bench = 'test/probes/' + name + '.bench.js'
   if (fs.existsSync(bench)) {
     task.bench = bench
   }
-
 })
 
 // make composite test tasks entries
@@ -88,19 +86,27 @@ const compositeTests = fs.readdirSync('test/composite')
 compositeTests.forEach(function (file) {
   if (!/.+\.test\.js$/.test(file)) return
 
-  // get only the name before .(test|bench).js
+  // get only the name before .test|bench.js
   const name = file.replace(/^(.+)\.[^\.]+\.js/, '$1')
   tasks['composite:' + name] = {
     test: 'test/composite/' + name + '.test.js'
   }
-
-  taskGroups.composite.push('composite:' + name)
 })
 
-// make unit. probes, composite tests
+// Create build tasks
+//makeBuildTask('build', 'lib/**/*.js')
+//makeBuildTask('build:probe', 'lib/probe/*.js')
+//Object.keys(tasks).forEach(function (name) {
+//  const task = tasks[name]
+//  if (task.lib) {
+//    makeBuildTask('build:' + name, task.lib)
+//  }
+//})
+
+// Create test tasks
+makeTestTask('test', 'test/**/*.test.js')
 Object.keys(tasks).forEach(function (name) {
   const task = tasks[name]
-  // make individual tests (unit:error, etc.) within each.
   if (task.test) {
     makeTestTask('test:' + name, task.test)
   }
@@ -115,60 +121,80 @@ Object.keys(tasks).forEach(function (name) {
 //  }
 //})
 
-/*
+
+// Create support-matrix tasks
+//require('./test/versions')
+//  .map(function (mod) {return mod.name})
+//  .forEach(makeMatrixTask)
+
+//gulp.task('support-matrix', function () {
+//  return spawn('alltheversions', ['--verbose'], {
+//    stdio: 'inherit'
+//  })
+//})
+
 // Create watcher task
-gulp.task('watch', function () {
-  Object.keys(tasks).forEach(function (name) {
-    if (name === 'probes') return
-    const task = tasks[name]
+//gulp.task('watch', function () {
+//  Object.keys(tasks).forEach(function (name) {
+//    if (name === 'probes') return
+//    const task = tasks[name]
+//
+//    const shouldBench = task.bench && !process.env.SKIP_BENCH
+//
+//    // These spawns tasks in a child processes. This is useful for
+//    // preventing state persistence between runs in a watcher and
+//    // for preventing crashes or exits from ending the watcher.
+//    function coverage () {
+//      return spawn('gulp', ['coverage:' + name], {
+//        stdio: 'inherit'
+//      })
+//    }
+//
+//    function bench () {
+//      return spawn('gulp', ['bench:' + name], {
+//        stdio: 'inherit'
+//      })
+//    }
+//
+//    function sequence (steps) {
+//      function next () {
+//        const step = steps.shift()
+//        const v = step()
+//        if (steps.length) {
+//          v.on('close', next)
+//        }
+//        return v
+//      }
+//      return next()
+//    }
+//
+//
+//    gulp.watch([ task.lib.replace(/^dist/, 'lib') ], [
+//      'build:' + name
+//    ])
+//
+//    gulp.watch([ task.lib ], function () {
+//      const steps = []
+//      if (task.test) steps.push(coverage)
+//      if (shouldBench) steps.push(bench)
+//      return sequence(steps)
+//    })
+//
+//    if (task.test) {
+//      gulp.watch([ task.test ], coverage)
+//    }
+//
+//    if (shouldBench) {
+//      gulp.watch([ task.bench ], bench)
+//    }
+//  })
+//})
 
-    const shouldBench = task.bench && !process.env.SKIP_BENCH
-
-    // These spawns tasks in a child processes. This is useful for
-    // preventing state persistence between runs in a watcher and
-    // for preventing crashes or exits from ending the watcher.
-    function coverage () {
-      return spawn('gulp', ['coverage:' + name], {
-        stdio: 'inherit'
-      })
-    }
-
-    function bench () {
-      return spawn('gulp', ['bench:' + name], {
-        stdio: 'inherit'
-      })
-    }
-
-    function sequence (steps) {
-      function next () {
-        const step = steps.shift()
-        const v = step()
-        if (steps.length) {
-          v.on('close', next)
-        }
-        return v
-      }
-      return next()
-    }
-
-    gulp.watch([ task.lib ], function () {
-      const steps = []
-      if (task.test) steps.push(coverage)
-      if (shouldBench) steps.push(bench)
-      return sequence(steps)
-    })
-
-    if (task.test) {
-      gulp.watch([ task.test ], coverage)
-    }
-
-    if (shouldBench) {
-      gulp.watch([ task.bench ], bench)
-    }
-  })
-})
-// */
-
+// Set default task to run the watcher
+//gulp.task('default', [
+//  'watch'
+//])
+gulp.task('default', ['test'])
 //
 // Helpers
 //
@@ -186,31 +212,25 @@ function tester () {
   })
 }
 
-/*
 function makeBuildTask (name, files) {
   const p = files.replace(/^dist/, 'lib')
   let d = files.slice(0, files.length - path.basename(files).length - 1)
   if (d === 'dist/**') d = 'dist'
-  gulp.task(name, ['dist/probes'], function () {
+  gulp.task(name, [/*'dist/probes'*/], function () {
     return gulp.src(p)
-      .pipe(gulp.dest(d))
+      //.pipe(gulp.dest(d))
   })
 }
-// */
-
-gulp.task('unit', gulp.series(taskGroups.unit.map(name => 'test:' + name)))
-gulp.task('probes', gulp.series(taskGroups.probes.map(name => 'test:' + name)))
 
 function makeTestTask (name, files) {
-  gulp.task(name, function (done) {
+  gulp.task(name, [/*'build'*/], function () {
     return gulp.src(files, {
       read: false
     })
       .pipe(tester())
-      .once('end', done)
+      .once('end', process.exit)
   })
 }
-
 
 //function makeCoverageTask (name, files, libs) {
 //  libs = libs || 'dist/**/*.js'
@@ -235,3 +255,14 @@ function makeTestTask (name, files) {
 //  })
 //}
 
+//function makeMatrixTask (name) {
+//  gulp.task('support-matrix:' + name, function () {
+//    return spawn('testeachversion', [
+//      '--package', name,
+//      '--verbose',
+//      '--suppress', 'false'
+//    ], {
+//      stdio: 'inherit'
+//    })
+//  })
+//}
