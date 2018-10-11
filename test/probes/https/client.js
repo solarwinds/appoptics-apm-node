d7fa0a4b7e1c10f9c5baa5e16b45c03bc02da91a
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-10-11 15:41:30 -0700

    Add node v8.9.0 workaround code.

--- test/probes/https/client.js
+++ test/probes/https/client.js
@@ -1,3 +1,5 @@
+'use strict'
+
 exports.data = function (ctx) {
   return {
     url: 'https://localhost:' + ctx.data.port + '/?foo=bar'
@@ -5,7 +7,18 @@ exports.data = function (ctx) {
 }
 
 exports.run = function (ctx, done) {
-  ctx.https.get(ctx.data.url, function (res) {
+  let options = {
+    ecdhCurve: 'auto',
+    protocol: 'https:',
+    host: 'localhost',
+    port: ctx.data.port,
+    path: '/?foo=bar'
+  }
+  // following fails on 8.9.0 - must use options above with ecdhCurve specified. That test
+  // fails because the port is missing in the remoteUrl property. Upgraded node to 8.12.0
+  // so simple URL works.
+  options = ctx.data.url
+  ctx.https.get(options, function (res) {
     res.resume()
     res.on('end', done.bind(null, null))
   }).on('error', done)
