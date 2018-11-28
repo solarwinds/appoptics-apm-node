f292406cdf0d0cf970ac2188da40420018766091
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-11-28 08:49:27 -0800

    test/composite - fix invalid header test

--- test/composite/request.test.js
+++ test/composite/request.test.js
@@ -171,7 +171,7 @@ describe('probes.request', function () {
       const xtrace = origin.toString().slice(0, 42) + '0'.repeat(16) + '01'
 
       const logChecks = [
-        {level: 'warn', message: 'invalid X-Trace header received: %s', values: [xtrace]},
+        {level: 'warn', message: `invalid X-Trace header received ${xtrace}`},
       ]
       helper.checkLogMessages(ao.debug, logChecks)
 
