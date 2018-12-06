03c384af665ad3bcee541491e46bc35a781a2716
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-12-06 11:20:22 -0800

    test/pg - explain naming

--- test/probes/pg.test.js
+++ test/probes/pg.test.js
@@ -3,6 +3,13 @@
 const semver = require('semver')
 const pkg = require('pg/package.json')
 
+//
+// so why are the files called pg6-minus and pg6-plus but
+// the test is for 7.0.0? because in theory v6 has all v7
+// features in it so could be tested against that both. we're
+// not doing that now to avoid the complications of interpreting
+// the results.
+//
 if (semver.lt(pkg.version, '7.0.0')) {
   require('./pg6-minus')
 } else {
