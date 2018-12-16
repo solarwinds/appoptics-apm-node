1d9e8a272925626c3531eb503df2de11671d2264
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-12-15 18:29:26 -0800

    mongoose - update for new versions
    - probes/mongoose
    -   remove non-existent addQueue() patch
    -   wip addQueue() patch fixes
    -   console.log for simple coverage view
    -   fix call method (call => apply)
    
    - test/mongoose
    -   real instrumentation tests
    -   callbacks and promises
    -   really test addQueue()
    -   should => expect
    -   first pass at aggregated messages

--- test/probes/mongoose.test.js
+++ test/probes/mongoose.test.js
@@ -1,83 +1,339 @@
 'use strict'
 
-const should = require('should')
+const expect = require('chai').expect
 const {ao} = require('../1.test-common.js')
-
-const Span = ao.Span
+const helper = require('../helper')
+const semver = require('semver')
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const pkg = require('mongoose/package')
 
-// use built-in Promise, replaceing mongoose's own implementation (deprecated)
+// use built-in Promise, replacing mongoose's own implementation (deprecated)
 mongoose.Promise = Promise
 
-let host = process.env.AO_TEST_MONGODB_2_4 || 'localhost:27017'
+let host = process.env.AO_TEST_MONGODB_3_0
+  || process.env.AO_TEST_MONGODB_2_6
+  || process.env.AO_TEST_MONGODB_2_4
+  || 'localhost:27017'
 
 if (process.env.CI === 'true' && process.env.TRAVIS === 'true') {
-  host = process.env.AO_TEST_MONGODB_3 || 'localhost:27017'
+  host = process.env.AO_TEST_MONGODB_3_0 || 'localhost:27017'
 }
 
+const major = semver.major(pkg.version)
+
+// use AO_IX if present. It provides a unique ID to prevent collisions
+// during matrix testing. It's not needed when testing only one instance
+// at a time locally.
+const dbn = 'test' + (process.env.AO_IX ? '-' + process.env.AO_IX : '')
+
 describe('probes/mongoose ' + pkg.version, function () {
-  const Cat = mongoose.model('test', new Schema({
+  const mongoOpts = major >= 5 ? {useNewUrlParser: true} : {useMongoClient: true}
+
+  const Cat = mongoose.model(dbn, new Schema({
     name: String
   }))
-  const mOpts = {useMongoClient: true}
+  let emitter
+  let backtraces
+  let fsenabled
 
-  before(function (done) {
+  before(function () {
     ao.g.testing(__filename)
-    mongoose.connect('mongodb://' + host + '/test', mOpts, done)
   })
   after(function () {
-    mongoose.disconnect()
-    const context = ao.requestStore.active
-    if (context) {
-      ao.resetRequestStore()
+  })
+
+  //
+  // Intercept appoptics messages for analysis
+  //
+  before(function (done) {
+    emitter = helper.appoptics(done)
+    ao.sampleRate = ao.addon.MAX_SAMPLE_RATE
+    ao.sampleMode = 'always'
+    // make them more readable
+    backtraces = ao.probes['mongodb-core'].collectBacktraces
+    ao.probes['mongodb-core'].collectBacktraces = false
+    // and don't let file IO complicate the results
+    fsenabled = ao.probes.fs.enabled
+    ao.probes.fs.enabled = false
+  })
+  after(function (done) {
+    ao.probes.fs.enabled = fsenabled
+    ao.probes['mongodb-core'].collectBacktraces = backtraces
+    emitter.close(done)
+  })
+
+  //
+  // define common checks
+  //
+  const check = {
+    base: function (msg) {
+      expect(msg).to.include({Spec: 'query'})
+      expect(msg).to.include({Flavor: 'mongodb'})
+      expect(msg).to.have.property('RemoteHost')
+      expect(msg.RemoteHost).to.match(/:\d*$/)
+    },
+    common: function (msg) {
+      expect(msg).to.include({Database: dbn})
+    },
+    entry: function (msg) {
+      expect(msg).to.include({Layer: 'mongodb-core'})
+      expect(msg).to.include({Label: 'entry'})
+      check.base(msg)
+    },
+    exit: function (msg) {
+      expect(msg).to.include({Layer: 'mongodb-core'})
+      expect(msg).to.include({Label: 'exit'})
     }
-  })
+  }
 
-  let savedCat
-
-  it('should trace through mongoose adding an object', function (done) {
-    const span = new Span('outer', {})
-    span.run(function () {
-      const data = {
-        name: 'Sargeant Cuddlesby'
+  // fake test to work around UDP dropped message issue
+  it('UDP might lose a message', function (done) {
+    helper.test(emitter, function (done) {
+      ao.instrument('fake', function () {})
+      done()
+    }, [
+      function (msg) {
+        expect(msg).to.have.property('Label').oneOf(['entry', 'exit'])
+        expect(msg).to.have.property('Layer', 'fake')
       }
-
-      ao.requestStore.set('name', data.name)
-
-      const kitty = new Cat(data)
-      kitty.save(function (err, item, rows) {
-        let name = ao.requestStore.get('name')
-        should.equal(name, data.name)
-        savedCat = item
-        Cat.findOne(data, function () {
-          name = ao.requestStore.get('name')
-          should.equal(name, data.name)
-          done()
-        })
-      })
-    })
+    ], done)
   })
 
-  it('should trace through mongoose deleting an object', function (done) {
-    const span = new Span('outer', {})
-    span.run(function () {
+  const cat1 = {
+    name: 'Sargeant Cuddlesby'
+  }
 
-      ao.requestStore.set('cat', 'Mimi')
+  const cat2 = {
+    name: 'HeiHei'
+  }
 
-      Cat.remove(function (err, deletedCat) {
-        let storedCat = ao.requestStore.get('cat')
-        should.equal(storedCat, 'Mimi')
-        Cat.findOne(savedCat, function (err, cat) {
-          storedCat = ao.requestStore.get('cat')
-          should.equal(storedCat, 'Mimi')
-          should.not.exist(cat)
-          done()
-        })
-      })
+  const cat3 = {
+    name: 'Mimi'
+  };
+
+  //
+  // the following tests basically check the same thing as the mongodb-core
+  // tests. mongoose itself is not instrumented; only mongodb-core is. but
+  // for mongodb-core to instrument context must be maintained through the
+  // mongoose api. that's what the mongoose probe does.
+  //
+
+  ['callback', 'promise'].forEach(type => {
+
+    //
+    // connect
+    //
+    it(`should connect correctly with a ${type}`, function (done) {
+      if (type === 'callback') {
+        mongoose.connect(`mongodb://${host}/${dbn}`, mongoOpts, done)
+      } else {
+        mongoose.connect(`mongodb://${host}/${dbn}`, mongoOpts)
+          .then(r => {
+            done()
+          })
+      }
     })
+
+
+    //
+    // add
+    //
+    function makeAddFunction (cat) {
+      return function addFunction (done) {
+        const kitty = new Cat(cat)
+
+        if (type === 'callback') {
+          kitty.save(function (err, item, rows) {
+            done()
+          })
+        } else {
+          kitty.save()
+            .then(k => done())
+        }
+      }
+    }
+
+    function testAddCat (done) {
+
+      function entry (msg) {
+        check.entry(msg)
+        check.common(msg)
+        expect(msg).to.include({QueryOp: 'insert'})
+        expect(msg.Insert_Document).to.be.a('String')
+        const q = JSON.parse(msg.Insert_Document)
+        expect(q).to.be.an('Array')
+        expect(q[0]).to.be.an('Object')
+        expect(q[0]).to.include(cat1)
+      }
+      function exit (msg) {
+        check.exit(msg)
+      }
+      const steps = [entry, exit]
+
+      helper.test(
+        emitter,
+        makeAddFunction(cat1),
+        steps,
+        done
+      )
+    }
+
+    it(`should add an object using a ${type}`, testAddCat)
+
+    //
+    // find
+    //
+
+    function makeFindFunction (cat) {
+      const findQuery = Object.assign({}, cat)
+      return function findFunction (done) {
+        if (type === 'callback') {
+          Cat.findOne(findQuery, function (err, item, rows) {done(err)})
+        } else {
+          Cat.findOne(findQuery)
+            .then(r => done())
+        }
+      }
+    }
+
+    function testFindCat (done) {
+      const findQuery = Object.assign({}, cat1)
+
+      function entry (msg) {
+        check.entry(msg)
+        check.common(msg)
+        expect(msg).to.include({QueryOp: 'find'})
+        expect(msg).to.include({Query: JSON.stringify(findQuery)})
+      }
+      function exit (msg) {
+        check.exit(msg)
+      }
+      const steps = [entry, exit]
+
+      helper.test(
+        emitter,
+        makeFindFunction(cat1),
+        steps,
+        done
+      )
+    }
+
+    it(`should find an object using a ${type}`, testFindCat)
+
+    //
+    // delete
+    //
+    const mongoDelete = major >= 5 ? 'deleteOne' : 'remove'
+
+    function makeDeleteFunction (cat) {
+      const deleteQuery = Object.assign({}, cat)
+      return function deleteFunction (done) {
+        if (type === 'callback') {
+          Cat[mongoDelete](deleteQuery, function (err, deletedCat) {done(err)})
+        } else {
+          Cat[mongoDelete](deleteQuery)
+            .then(deletedCat => done())
+        }
+      }
+    }
+
+
+    function testDeleteCat (done) {
+      const deleteQuery = JSON.stringify([cat1])
+
+      function entry (msg) {
+        check.entry(msg)
+        check.common(msg)
+        expect(msg).to.include({QueryOp: 'remove'})
+        expect(msg).to.include({Query: deleteQuery})
+      }
+      function exit (msg) {
+        check.exit(msg)
+      }
+      const steps = [entry, exit]
+
+      helper.test(
+        emitter,
+        makeDeleteFunction(cat1),
+        steps,
+        done
+      )
+    }
+
+    it(`should delete an object using a ${type}`, testDeleteCat)
+
+    //
+    // disconnect
+    //
+    it(`should disconnect correctly with a ${type}`, function (done) {
+      if (type === 'callback') {
+        mongoose.disconnect(done)
+      } else {
+        mongoose.disconnect()
+          .then(r => {
+            done()
+          })
+      }
+    })
+
+    //
+    // force addQueue by not waiting for connect() to complete.
+    //
+    it(`should connect and queue queries using a ${type}`, function (done) {
+      mongoose.connect(`mongodb://${host}/${dbn}`, mongoOpts)
+        .then(r => {
+          ao.loggers.debug('addQueue - connected')
+        })
+
+      function entry1 (msg) {
+        check.entry(msg)
+        check.common(msg)
+        expect(msg).to.include({QueryOp: 'insert'})
+        expect(msg.Insert_Document).to.be.a('String')
+
+        const q = JSON.parse(msg.Insert_Document)
+        expect(q).to.be.an('Array')
+        expect(q[0]).to.be.an('Object')
+        expect(q[0]).to.include(cat1)
+      }
+      function exit1 (msg) {
+        check.exit(msg)
+      }
+      function noop () {}
+      const steps = [entry1, noop, noop, exit1, noop, noop]
+
+      ao.g.stop = true
+
+      helper.test(
+        helper.setAggregate(emitter),
+        function (done) {
+          let n = 0
+          function three () {
+            ao.loggers.debug('addQueue - three() %d', n + 1)
+            if (++n >= 3) {
+              //mongoose.disconnect().then(r => done())
+              done()
+            }
+          }
+          makeAddFunction(cat1)(three)
+          makeAddFunction(cat2)(three)
+          makeAddFunction(cat3)(three)
+          //makeFindFunction(cat1)(three)
+          //makeDeleteFunction(cat1)(three)
+        },
+        steps,
+        function testOver (err, messages) {
+          expect(messages.length).to.equal(steps.length + 2)
+          helper.clearAggregate(emitter)
+          debugger
+          done()
+        }
+      )
+    })
+
   })
+
 })
