3b3ca96e29c343ff8613272f1ddf23571bb1f14f
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-03-21 13:27:00 -0700

    test/versions - include mongoose 4.11.0 again

--- test/versions.js
+++ test/versions.js
@@ -74,7 +74,8 @@ test('memcached', '>= 2.2.0')
 
 test('mongodb-core', '>= 2.0.0')
 
-test('mongoose', '>= 4.6.4 < 4.11.0 || >= 4.11.1')
+//test('mongoose', '>= 4.6.4 < 4.11.0 || >= 4.11.1')
+test('mongoose', '>= 4.6.4')
 
 test('mysql', '>= 2.1.0')
 test('oracledb', '>= 2.0.14')
