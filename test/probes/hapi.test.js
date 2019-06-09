8d7ccefd8fb7896b42546cbf5482e7cc9caee9f1
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-06-09 06:53:47 -0700

    support @hapi scoped repo for hapi and vision
    - lib/require-patch - create name => patch file map
    - lib/require-patch - allow array return with version
    - lib/probes/amqplib - handle name argument for callback_api
    
    probes/amqplib - fix to handle name argument

--- test/probes/hapi.test.js
+++ test/probes/hapi.test.js
@@ -3,6 +3,10 @@
 const semver = require('semver')
 const pkg = require('hapi/package.json')
 
+// if the @hapi test ran first clear the environment variable so this
+// test won't load @hapi.
+delete process.env.hapiVersion
+
 // this test is split into two separate files so that one can use async/await syntax.
 // the javascript parser will flag syntax errors if it encounters async/await prior to
 // versions that support it.
