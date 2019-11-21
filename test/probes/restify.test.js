0343beaa76f73f506d5b1cb8368fbc63789eb101
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-11-21 06:23:51 -0800

    test/probes/restify - debugging
    - turn off backtraces (too noisy)
    - decode graph if cls-hooked.getGraph() available

--- test/probes/restify.test.js
+++ test/probes/restify.test.js
@@ -28,9 +28,10 @@ if (semver.satisfies(process.version, '>=8.0.0')) {
   ao.probes.fs.enabled = false
 }
 
-describe('probes.restify ' + pkg.version, function () {
+describe(`probes.restify ${pkg.version}`, function () {
   let emitter
   let fsState
+  let previousTraces;
 
   //
   // Intercept appoptics messages for analysis
@@ -42,11 +43,20 @@ describe('probes.restify ' + pkg.version, function () {
     // restify newer versions of restify use negotiator which does file io
     fsState = ao.probes.fs.enabled
     ao.probes.fs.enabled = false
+    previousTraces = ao.probes.restify.collectBacktraces;
+    ao.probes.restify.collectBacktraces = false;
     ao.g.testing(__filename)
   })
   after(function (done) {
     emitter.close(done)
+    if (ao.requestStore.getGraph) {
+      process.on('exit', function () {
+        ao.requestStore._hook.disable();
+        interpretContextGraph(ao.requestStore.getGraph());
+      })
+    }
     ao.probes.fs.enabled = fsState
+    ao.probes.restify.collectBacktraces = previousTraces;
   })
 
   const check = {
@@ -115,7 +125,8 @@ describe('probes.restify ' + pkg.version, function () {
       }
     ]
     helper.doChecks(emitter, validations, function () {
-      server.close(done)
+      server.close(done);
+      server.removeAllListeners('request');
     })
 
     const server = app.listen(function () {
@@ -125,7 +136,7 @@ describe('probes.restify ' + pkg.version, function () {
   }
 
   function testMiddleware (done) {
-    const app = restify.createServer(opts)
+    const app = restify.createServer(opts);
 
     app.get('/hello/:name', function renamer (req, res, next) {
       req.name = req.params.name
@@ -165,7 +176,8 @@ describe('probes.restify ' + pkg.version, function () {
       }
     ]
     helper.doChecks(emitter, validations, function () {
-      server.close(done)
+      server.close(done);
+      server.removeAllListeners('request');
     })
 
     const server = app.listen(function () {
@@ -174,7 +186,7 @@ describe('probes.restify ' + pkg.version, function () {
     })
   }
 
-  if (semver.satisfies(process.version.slice(1), '> 0.8')) {
+  if (semver.gte(process.version, '6.0.0')) {
     it('should forward controller/action', testControllerAction)
     it('should create a span for each middleware', testMiddleware)
   } else {
@@ -182,3 +194,48 @@ describe('probes.restify ' + pkg.version, function () {
     it.skip('should create a span for each middleware', testMiddleware)
   }
 })
+
+function interpretContextGraph (graph) {
+  const inits = graph.inits;
+  const asyncIds = Reflect.ownKeys(graph.inits);
+  let firstNonBootstrapId = asyncIds[asyncIds.length - 1];
+
+  // find the first non-bootstrap ID
+  for (let i = 0; i < asyncIds.length; i++) {
+    if (!inits[asyncIds[i]].bootstrap) {
+      firstNonBootstrapId = asyncIds[i];
+      break;
+    }
+  }
+  console.log('first non-bootstrap ID', firstNonBootstrapId);
+
+  // are any missing inits from non-bootstrap asyncIds?
+  ['beforeNoInit', 'afterNoInit', 'destroyNoInit'].forEach(error => {
+    const bad = graph.errors[error].filter(asyncId => asyncId >= firstNonBootstrapId);
+    if (bad.length) {
+      console.log(`non-bootstrap ${error} ${bad}`);
+    }
+  });
+
+  const odd = asyncIds.filter(id => {
+    return !inits[id].bootstrap && (inits[id].befores !== inits[id].afters || inits[id].inits !== inits[id].destroys);
+  });
+
+  if (odd.length) {
+    console.log('asymmetric pairings');
+    odd.forEach(id => {
+      const short = {
+        t: inits[id].type,
+        i: inits[id].inits,
+        b: inits[id].befores,
+        a: inits[id].afters,
+        d: inits[id].destroys,
+        tid: inits[id].triggerId,
+        eaID: inits[id].eaID,
+      }
+      console.log(id, short);
+    })
+  }
+
+  //console.log(JSON.stringify(graph));
+}
