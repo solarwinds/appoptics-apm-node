70580845904ea464af81d4f99ffcd2e0dd9e2b34
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-12-13 10:40:31 -0800

    test/ - refactor http/https testing
    - use same code for http/https testing
    - http-common - new common testing
    - http.test.js, https.test.js - simple wrappers
    - single http directory of support files

--- test/probes/http/client-object.js
+++ test/probes/http/client-object.js
@@ -1,3 +1,5 @@
+'use strict';
+
 exports.data = function (ctx) {
   return {
     hostname: 'localhost',
--- test/probes/http/stream.js
+++ test/probes/http/stream.js
@@ -1,11 +1,11 @@
+'use strict';
+
 exports.data = function (ctx) {
-  return {
-    url: 'http://localhost:' + ctx.data.port + '/?foo=bar'
-  }
+  return {url: `${ctx.p}://localhost:${ctx.data.port}/?foo=bar`};
 }
 
 exports.run = function (ctx, done) {
-  var req = ctx.http.get(ctx.data.url)
+  const req = ctx.http.get(ctx.data.url)
   req.on('response', function (res) {
     res.resume()
     res.on('end', done.bind(null, null))
