92e61725dd73a14555f42a80ca0c2cb76d241211
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-02 08:32:03 -0800

    get versions synchronously

--- test/lambda/get-lambda-version.js
+++ test/lambda/get-lambda-version.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const fsp = require('fs').promises;
+const fs = require('fs');
 
 //
 // this is modified from the apm-node-lambda-layer/util/get-lambda-version.js
 //
 // the main difference is that this does not write the modified version into package.json.
 //
+// it also reads synchronously and doesn't write to the console.
+//
 // append '-version-suffix' to the version and return that.
 //
-async function getLambdaVersion (file) {
-  const bytes = await fsp.readFile(file, 'utf8');
+function getLambdaVersion (file) {
+  const bytes = fs.readFileSync(file, 'utf8');
   const pkg = JSON.parse(bytes);
 
   // lambda releases will be numbered by the existing version number with a
