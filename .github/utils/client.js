33965d070fe1e15db8c9ef44d8dade0f2f22462b
Ron Ilan <github@ronilan.com>
2021-08-25 14:03:32 -0700

    Modified both client and server to accommodate various scenarios when running in GitHub Actions. Server emits error if not instrumented. Client emits error if fails to connect to server after set number of trys. Simple it is no more...

--- .github/utils/client.js
+++ .github/utils/client.js
@@ -1,15 +1,17 @@
-// a simple "automated client to make repeated http requests using the 4 verbs
+// a simple "automated client" to make repeated http requests using the 4 verbs
 const fetch = require('node-fetch')
+const arg = require('arg')
+
+const args = arg({ '--pace': Number, '--total': Number, '--trys': Number })
+
+const pace = args['--pace'] || 1 // requests per second
+let total = args['--total'] || 1 // how many tomes to request all 4 verbs
+let trys = args['--trys'] || 10 // max number of trys to connect to server (prevents CI hangups)
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-const total = parseInt(process.argv[2], 10) || 1 // how many tomes to request all 4 verbs
-const pace = parseInt(process.argv[3], 10) || 1 // requests per second
-
-const run = async (total = 1, pace = 1) => {
-  let count = 0
-
+const run = async () => {
   const sleep = async (ms = 0) => {
     await new Promise(resolve => setTimeout(resolve, ms))
   }
@@ -18,49 +20,59 @@ const run = async (total = 1, pace = 1) => {
     let res
     let body
 
-    count += 1
-    console.log(`${count} of ${total}`)
+    console.log(`Will request ${total} times`)
+    total -= 1
 
     // don't do much other than hit the endpoint with the verb
     res = await fetch(`http://${hostname}:${port}/`)
     body = await res.text()
-    console.log(body)
+    console.log(`Sent GET. Server replied: ${body}`)
     await sleep(1000 / pace)
 
     res = await fetch(`http://${hostname}:${port}/`, { method: 'POST', body: 'key=value' })
     body = await res.text()
-    console.log(body)
+    console.log(`Sent POST. Server replied: ${body}`)
     await sleep(1000 / pace)
 
     res = await fetch(`http://${hostname}:${port}/`, { method: 'PATCH', body: 'key=value' })
     body = await res.text()
-    console.log(body)
+    console.log(`Sent PATCH. Server replied: ${body}`)
     await sleep(1000 / pace)
 
     res = await fetch(`http://${hostname}:${port}/`, { method: 'DELETE' })
     body = await res.text()
-    console.log(body)
+    console.log(`Sent DELETE. Server replied: ${body}`)
     await sleep(1000 / pace)
 
-    if (count < total) runOnce()
+    if (total > 0) runOnce()
   }
 
   const waitForServer = async () => {
     try {
       await fetch(`http://${hostname}:${port}/`)
-      console.log('server is live :)')
+      console.log('Connected to server')
 
       runOnce()
     } catch {
-      console.log('server not live yet...')
-
+      console.log(`Server not responding yet... will try ${trys} times`)
+      trys -= 1
       // wait a sec and try again
       await sleep(1000)
-      waitForServer()
+      if (trys > 0) await waitForServer()
     }
   }
 
-  waitForServer()
+  await waitForServer()
+  if (trys <= 0) throw new Error('Failed to connect to server')
 }
 
-run(total, pace)
+// iife invokes an async function with try/catch
+// used to manage the UnhandledPromiseRejectionWarning warning
+(async function () {
+  try {
+    await run()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
