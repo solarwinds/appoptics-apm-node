0b535f571e630a215e75a9f093f7a16fe8ab7a40
Ron Ilan <github@ronilan.com>
2022-06-14 11:58:59 -0700

    Removed node-cassandra-cql probe file and dead test files. Deleted config settings. Tests disabled and dev dependency removed years ago.

--- test/probes/node-cassandra-cql/basic.js
+++
@@ -1,5 +0,0 @@
-exports.run = function (ctx, done) {
-  ctx.cql.execute('SELECT now() FROM system.local', function () {
-    done()
-  })
-}
--- test/probes/node-cassandra-cql/sanitize.js
+++
@@ -1,8 +0,0 @@
-exports.run = function (ctx, done) {
-  const conf = ctx.ao.probes['node-cassandra-cql']
-  conf.sanitizeSql = true
-  ctx.cql.execute("SELECT * from foo where bar='1'", function (err) {
-    conf.sanitizeSql = false
-    done(err)
-  })
-}
